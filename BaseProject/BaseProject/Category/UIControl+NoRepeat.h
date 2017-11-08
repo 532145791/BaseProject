d075cd0e1d01bcaebdd09b4cb41c56c210c854f6
冷超 <lc@lengchaodeMacBook-Pro.local>
2017-11-08 14:05:12 +0800

    防止按钮重复点击

---
+++ BaseProject/BaseProject/Category/UIControl+NoRepeat.h
@@ -0,0 +1,17 @@
+//
+//  UIControl+NoRepeat.h
+//  test
+//
+//  Created by NeverMore on 2017/11/8.
+//  Copyright © 2017年 lengchao. All rights reserved.
+//
+
+//---------------------------防重复点击--------------------------//
+
+#import <UIKit/UIKit.h>
+#import <objc/runtime.h>
+#define defaultInterval 0.5//默认时间间隔
+@interface UIControl (NoRepeat)
+@property(nonatomic,assign) NSTimeInterval timeInterval;//用这个给重复点击加间隔
+@property(nonatomic,assign) BOOL isIgnoreEvent;//YES不允许点击NO允许点击
+@end
